c3060d35fbf059187cfaa87cbad3e1146331861b
Petr Lidzhiev <petyaposk@gmail.com>
2024-03-28 14:28:32 +0300

    end diplom

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,12 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator,\
-                                    RegexValidator,\
-                                    MaxValueValidator
+from django.core.validators import (
+    MinValueValidator,
+    RegexValidator,
+    MaxValueValidator
+)
 from django.db import models
 
-from .constant import LENTH_COLOR, MAX_LENGTH,\
-                        MAX_VALUE, MIN_VALUE
+from .constant import (
+    LENTH_COLOR,
+    MAX_LENGTH,
+    MAX_VALUE,
+    MIN_VALUE
+)
 
 User = get_user_model()
 
